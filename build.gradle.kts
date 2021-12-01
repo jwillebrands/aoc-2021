4bea8c12aa19ce5794fd0443669858de8a9ed941
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2021-12-01 14:19:08 +0100

    day 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.kotest:kotest-assertions-core:5.0.1")
 }
 
 tasks.test {
