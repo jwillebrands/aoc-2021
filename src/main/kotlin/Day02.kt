03377bedef71e9ba6ee768a3fa3e180f2047d074
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2021-12-05 11:59:41 +0100

    refactor + implement day 2 part 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,24 +1,33 @@
 import java.io.File
 
-data class Submarine(val hpos: Int = 0, val depth: Int = 0) {
-    fun move(direction: String, amount: Int): Submarine {
-       return when (direction) {
-            "forward" -> copy(hpos = hpos + amount)
-            "up" -> copy(depth = depth - amount)
-            "down" -> copy(depth = depth + amount)
-            else -> throw IllegalArgumentException("Unkown direction ${direction}")
-        }
-    }
+data class Submarine(val hpos: Int = 0, val depth: Int = 0, val aim: Int = 0)
+
+typealias SubmarineCommand = (Submarine, Int) -> Submarine
+typealias CommandFactory = (String) -> SubmarineCommand
+
+fun executeCommands(lines: List<String>, commandFactory: CommandFactory): Submarine {
+  return lines.map{it.split(" ")}
+    	.fold(Submarine()) {sub, cmd -> commandFactory(cmd[0])(sub, cmd[1].toInt())}
 }
 
 fun solvePart1(lines: List<String>): Int {
-    return with(lines.map{it.split(" ")}
-    	.fold(Submarine()) {sub, cmd -> sub.move(cmd[0], cmd[1].toInt())}
-	){ hpos * depth}
+    val commandFactory: CommandFactory = {command -> when(command) {
+      "forward" -> {sub, amount -> sub.copy(hpos = sub.hpos + amount)}
+      "up" -> {sub, amount -> sub.copy(depth = sub.depth - amount)}
+      "down" -> {sub, amount -> sub.copy(depth = sub.depth + amount)}
+      else -> throw IllegalArgumentException("Unknown command ${command}")
+    }}
+    return with(executeCommands(lines, commandFactory)){ hpos * depth}
 }
 
-fun solvePart2(lines: List<String>): String {
-  return ""
+fun solvePart2(lines: List<String>): Int {
+  val commandFactory: CommandFactory = {command -> when(command) {
+    "forward" -> {sub, amount -> sub.copy(hpos = sub.hpos + amount, depth = sub.depth + sub.aim * amount)}
+    "up" -> {sub, amount -> sub.copy(aim = sub.aim - amount)}
+    "down" -> {sub, amount -> sub.copy(aim = sub.aim + amount)}
+    else -> throw IllegalArgumentException("Unknown command ${command}")
+  }}
+  return with(executeCommands(lines, commandFactory)){ hpos * depth}
 }
 
 fun main() {
