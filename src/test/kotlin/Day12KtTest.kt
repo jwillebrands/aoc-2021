2aba3f8e66034deea446a24c2bc5874b24c04684
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2021-12-16 17:15:34 +0100

    formatting

--- src/test/kotlin/Day12KtTest.kt
+++ src/test/kotlin/Day12KtTest.kt
@@ -1,30 +1,30 @@
 package day12
 
 import io.kotest.matchers.shouldBe
+import java.util.stream.Stream
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
 
 @TestInstance(PER_CLASS)
 internal class Day12KtTest {
-    @ParameterizedTest
-    @MethodSource("testData")
-    fun `test part 1 sample input`(testData: TestData) {
-        solvePart1(testData.lines) shouldBe testData.part1Expectation
-    }
+  @ParameterizedTest
+  @MethodSource("testData")
+  fun `test part 1 sample input`(testData: TestData) {
+    solvePart1(testData.lines) shouldBe testData.part1Expectation
+  }
 
-    @ParameterizedTest
-    @MethodSource("testData")
-    fun `test part 2 sample input`(testData: TestData) {
-        solvePart2(testData.lines) shouldBe testData.part2Expectation
-    }
+  @ParameterizedTest
+  @MethodSource("testData")
+  fun `test part 2 sample input`(testData: TestData) {
+    solvePart2(testData.lines) shouldBe testData.part2Expectation
+  }
 
-    fun testData(): Stream<TestData> {
-        return Stream.of(
-            TestData(
-                """
+  fun testData(): Stream<TestData> {
+    return Stream.of(
+        TestData(
+            """
                 start-A
                 start-b
                 A-c
@@ -32,10 +32,13 @@ internal class Day12KtTest {
                 b-d
                 A-end
                 b-end
-                """.trimIndent().split("\n"), 10, 36
-            ),
-            TestData(
                 """
+                .trimIndent()
+                .split("\n"),
+            10,
+            36),
+        TestData(
+            """
                 dc-end
                 HN-start
                 start-kj
@@ -46,10 +49,13 @@ internal class Day12KtTest {
                 kj-sa
                 kj-HN
                 kj-dc
-                """.trimIndent().split("\n"), 19, 103
-            ),
-            TestData(
                 """
+                .trimIndent()
+                .split("\n"),
+            19,
+            103),
+        TestData(
+            """
                 fs-end
                 he-DX
                 fs-he
@@ -68,10 +74,16 @@ internal class Day12KtTest {
                 zg-he
                 pj-fs
                 start-RW
-                """.trimIndent().split("\n"), 226, 3509
-            )
-        )
-    }
+                """
+                .trimIndent()
+                .split("\n"),
+            226,
+            3509))
+  }
 
-    data class TestData(val lines: List<String>, val part1Expectation: Int, val part2Expectation: Int)
+  data class TestData(
+      val lines: List<String>,
+      val part1Expectation: Int,
+      val part2Expectation: Int
+  )
 }
